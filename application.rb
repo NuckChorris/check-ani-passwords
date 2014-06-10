adc1088bea42e86d37abb86bc40c58cfccc3ca0e
Peter Lejeck <peter.lejeck@gmail.com>
2014-06-10 02:48:54 -0700

    Should be fully twerking now

--- application.rb
+++ application.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'bundler/setup'
 require 'haml'
 require 'sinatra'
+require 'redis'
+
+redis = Redis.new(:url => ENV["REDISTOGO_URL"])
 
 configure do
   set :views, File.join(File.dirname(__FILE__), "views")
@@ -9,4 +12,11 @@ end
 
 get '/' do
   haml :root
+end
+post '/' do
+  if redis.sismember("EMAIL", params[:username])
+    haml :bad
+  else
+    haml :good
+  end
 end
